09da2f66138459b0e7f314dc78172a7f8ad85146
TK742 <ttasca742@gmail.com>
2022-09-20 02:08:14 -0300

    BaSP-W04-2022: solved strings, part 4

--- Semana 4/exercises/02_strings.js
+++ Semana 4/exercises/02_strings.js
@@ -6,3 +6,6 @@ console.log(firstFiveCharacters);
 
 var lastThreeCharacters = longString.substring(longString.length - 3);
 console.log(lastThreeCharacters);
+
+var firstLetterIsCapital = longString.substring(0, 1).toUpperCase() + longString.substring(1).toLowerCase();
+console.log(firstLetterIsCapital);
