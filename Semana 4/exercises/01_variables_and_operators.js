0e905fef02e509b0fb055e2000e8613698bb4535
TK742 <ttasca742@gmail.com>
2022-09-20 00:11:39 -0300

    BaSP-W04-2022: solved variables and operators, exercise 3. section finished

--- Semana 4/exercises/01_variables_and_operators.js
+++ Semana 4/exercises/01_variables_and_operators.js
@@ -1,11 +1,13 @@
 var a = 2;
 var b = 3;
 var c = a + b;
-
 console.log(c);
+
 
 var x = "string 1";
 var y = "string 2";
 var z = x + y;
+console.log(z);
 
-console.log(c);
+var length = x.length + y.length;
+console.log(length);
