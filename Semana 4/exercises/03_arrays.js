8ae75853c3fee3bd8164889a1fc4c305be492006
TK742 <ttasca742@gmail.com>
2022-09-20 17:53:38 -0300

    BaSP-W04-2022: arrays, second exercise

--- Semana 4/exercises/03_arrays.js
+++ Semana 4/exercises/03_arrays.js
@@ -1,3 +1,5 @@
 var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 console.log(months[5]);
 console.log(months[11]);
+
+console.log(months.sort());
