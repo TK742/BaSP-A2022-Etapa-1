790994a330cc7b84136fe96b7b1c7f022c88ebdd
TK742 <ttasca742@gmail.com>
2022-09-20 02:09:49 -0300

    BaSP-W04-2022: solved strings, part 5

--- Semana 4/exercises/02_strings.js
+++ Semana 4/exercises/02_strings.js
@@ -1,4 +1,4 @@
-var longString = "helloWorldOfWondersHowAreYouDoingMyDears?"
+var longString = "hello, World Of Wonders! How Are You Doing, My Dears?"
 console.log(longString.toUpperCase());
 
 var firstFiveCharacters = longString.substring(0, 5);
@@ -9,3 +9,5 @@ console.log(lastThreeCharacters);
 
 var firstLetterIsCapital = longString.substring(0, 1).toUpperCase() + longString.substring(1).toLowerCase();
 console.log(firstLetterIsCapital);
+
+console.log(longString.indexOf(' '));
