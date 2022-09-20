c05b9649e33c8dfba0cb4280a2171aee0bac748b
TK742 <ttasca742@gmail.com>
2022-09-20 20:10:47 -0300

    BaSP-W04-2022: ifelse, completed

--- Semana 4/exercises/04_if_else.js
+++ Semana 4/exercises/04_if_else.js
@@ -5,3 +5,22 @@ if (randomNumber >= 0.5) {
 else {
     console.log("Less than 0.5");
 }
+
+var age = randomNumber * 100;
+console.log(age);
+
+if (age < 2){
+    console.log("Bebé");
+} else if (age > 2 && age < 13) {
+    console.log("Niño");
+} else if (age > 13 && age < 20) {
+    console.log("Adolescente");
+} else if (age > 20 && age < 30) {
+    console.log("Joven");
+} else if (age > 30 && age < 60) {
+    console.log("Adulto");
+} else if (age > 60 && age < 75) {
+    console.log("Adulto mayor");
+} else {
+    console.log("Anciano");
+}
